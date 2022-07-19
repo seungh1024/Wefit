f9f3d3ed3cbf54c330569bf69724f76113723c0f
김진산 <lazybones1@naver.com>
2022-07-20 08:54:19 +0900

    Deleted springjwt/gradle/wrapper/gradle-wrapper.jar, springjwt/gradle/wrapper/gradle-wrapper.properties, springjwt/src/main/java/com/example/springjwt/dto/User.java, springjwt/src/main/java/com/example/springjwt/enums/RoleType.java, springjwt/src/main/java/com/example/springjwt/handler/CustomLoginSuccessHandler.java, springjwt/src/main/java/com/example/springjwt/repository/UserRepository.java, springjwt/src/main/java/com/example/springjwt/security/CustomAuthenticationFilter.java, springjwt/src/main/java/com/example/springjwt/security/CustomAuthenticationProvider.java, springjwt/src/main/java/com/example/springjwt/security/WebSecurityConfig.java, springjwt/src/main/java/com/example/springjwt/service/UserDetailsServiceImpl.java, springjwt/src/main/java/com/example/springjwt/SpringjwtApplication.java, springjwt/src/main/resources/application.properties, springjwt/src/test/java/com/example/springjwt/SpringjwtApplicationTests.java, springjwt/.gitignore, springjwt/build.gradle, springjwt/gradlew, springjwt/gradlew.bat, springjwt/settings.gradle

--- springjwt/src/main/java/com/example/springjwt/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springjwt.repository;
-
-import com.example.springjwt.dto.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-}
