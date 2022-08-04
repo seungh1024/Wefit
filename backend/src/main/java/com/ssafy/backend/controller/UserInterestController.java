fae888e24e0e1c57bb337c8fde35a1683efb93a1
dugazada <77961413+dugazada@users.noreply.github.com>
2022-08-04 23:48:19 +0900

    Feat-back : 관심사CRUD 진행 중

---
+++ backend/src/main/java/com/ssafy/backend/controller/UserInterestController.java
@@ -0,0 +1,39 @@
+package com.ssafy.backend.controller;
+
+import com.ssafy.backend.entity.Interest;
+import com.ssafy.backend.entity.UserInterest;
+import com.ssafy.backend.service.UserInterestService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class UserInterestController {
+
+    private final UserInterestService userInterestService;
+
+
+    public UserInterestController(UserInterestService userInterestService) {
+        this.userInterestService = userInterestService;
+    }
+
+    //사용자가 선택한 관심사 정보 조회
+    @GetMapping("/like/{username}")
+    public ResponseEntity<List<Interest>> getUserInterestInfo(@PathVariable("username") String username){
+        return new ResponseEntity<>(userInterestService.getUserInterest(username), HttpStatus.OK);
+    }
+
+    @PostMapping("/like/{username}")
+    public ResponseEntity<?> updateUserInterestInfo(@PathVariable("username") String username, @RequestBody List<Interest> interestList){
+        System.out.println(username);
+        System.out.println(interestList);
+        return new ResponseEntity<>(userInterestService.updateUserInterest(username,interestList), HttpStatus.OK);
+
+
+    }
+
+
+}
