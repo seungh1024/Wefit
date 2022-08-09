88bff56139857dcec9c48f8c1dab0d91273391c3
조성환 <chosw0812@naver.com>
2022-08-10 02:04:17 +0900

    회원가입 변경

--- frontend/src/views/Accounts/SignupByEmailView.vue
+++ frontend/src/views/Accounts/SignupByEmailView.vue
@@ -1,5 +1,5 @@
 <template>
- <div class="signupbyemail">
+ <!-- <div class="signupbyemail">
 		<form class="signupbyemailform">
 		<h2>회원가입 페이지</h2>
 			<p>
@@ -20,15 +20,50 @@
 				</div>
 				<input type="password" id="passwordInput" name="userPasswordCheck" class="input_text" ref="passwordInput" v-model.trim="userPasswordCheck" placeholder="패스워드를 확인하세요." />
 			</p>
-			<!-- 밑의 체크박스 넣어야합니까? -->
             <div>                
                 <input type = "checkbox"> <label>i agree 어쩌구</label>
             </div>
             <div>
                 <button @click.prevent = "emailSignup" id = "btn"> create your account</button>
             </div>
-            </form>
- </div>
+        </form>
+ </div> -->
+<div class="Back">
+    <div class="login">
+      <div class="background">
+      <div class="top-box"></div>
+      <div class="d-flex justify-content-center">
+        <h2>회원가입 페이지</h2>
+      </div>
+      <div class="bottom-box"></div>
+      <form class="loginform">
+
+          <div>
+            <label for="emailInput" id = "emailtext" class = "text">이메일</label>
+            </div>
+            <input type="text" id="emailInput" name="userEmail" class="input_text" ref="emailInput" v-model.trim="userEmail" placeholder="이메일을 입력하세요." />
+        <div>
+          <div>
+				<label for="passwordInput" id = "passwordtext" class = "text">비밀번호</label>
+				</div>
+				<input type="password" id="passwordInput" name="userPassword" class="input_text" ref="passwordInput" v-model.trim="userPassword" placeholder="패스워드를 입력하세요." />
+			  </div>
+              <div>
+                <div>
+				<label for="passwordInput" id = "passwordtext" class = "text">비밀번호 확인</label>
+				</div>
+				<input type="password" id="passwordInput" name="userPasswordCheck" class="input_text" ref="passwordInput" v-model.trim="userPasswordCheck" placeholder="패스워드를 확인하세요." />
+              </div>
+        <div class="buttons">
+          <button @click.prevent="login" class="button blue" id = "loginbtn">회원가입</button>
+          <div class="center_box">
+
+          </div>
+        </div>
+		  </form>
+    </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -76,32 +111,97 @@ export default {
     }
 }
 </script>
-
 <style scoped>
-.signupbyemail{
-    height: 100vh;
-    background-image: url("@/assets/loginpage.jpg");
-    background-repeat : no-repeat;
-    background-size : cover;
- }
-.signupbyemailform{
+.title{
+  color:white;
+  width: 100%;
+}
+.top-box{
+  height: 65px;
+}
+.bottom-box{
+  height: 60px;
+}
+
+.fw_su_box{
+  width: 55%;
+  display: flex;
+  justify-content: space-between;
+}
+.center_box{
+  display: flex;
+  justify-content: center;
+  margin-bottom: 10px;
+}
+
+.background{
+  height: 660px;
+  width: 500px;
+  overflow: hidden;
+  margin:0;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-image:linear-gradient(
+    rgba(0, 0, 0, 0),
+        rgba(255, 255, 255, 0)
+      ), url("@/assets/MainPage/menu_bg4.png");
+}
+.login{
+  width:80%; 
+  min-width: 500px;
+  margin:0 auto; 
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.Back{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background-color: rgba(30, 30, 30, 6);
+}
+
+ .loginform{
   text-align:center;
-  position : relative;
-  top :25%;
 }
-h2{
-    color :red;
-}
-.text{
-    color :white;
-}
-.input_text{
+
+ h2{
+	color : white;
+ }
+
+ #signup {
+  font-weight: bold;
+  color: white;
+  cursor:pointer;
+ }
+ #findid {
+  font-weight: bold;	
+  color: white;
+  cursor:pointer;
+ }
+ #findpw{
+  font-weight: bold;
+  color: white;
+  cursor:pointer;
+ }
+ #emailtext{
+  color: white;
+ }
+ #passwordtext{
+  color: white;
+ }
+ input{
   border-radius: 8px;
-  width : 24%;
+  width: 315px;
   height: 40px;
-  margin-left: 10px;
-}
-#btn{
+ }
+ input::placeholder{
+  padding-left: 10px;
+ }
+ #loginbtn{
     text-size-adjust: none;
     -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
     appearance: none;
@@ -111,16 +211,24 @@ h2{
     user-select: none;
     font-family: -apple-system, BlinkMacSystemFont, Roboto, "Droid Sans", "Helvetica Neue", "Apple SD Gothic Neo", "sans-serif", sans-serif;
     display: block;
-    width: 23%;
+    width: 315px;
     height: 46px;
     padding-bottom: 1px;
     border-radius: 10px;
     cursor: pointer;
-    color: #fff;
+    color: rgb(30,30,30);
     font-weight: bold;
-    background: #ef3e43;
+    background: rgb(217, 188, 128);
     border: 0;
     margin-top: 30px;
     font-size: 17px;
+    margin-bottom : 10px;
+ }
+checkbox{
+    padding:0px;
+    margin:0px
+}
+.checkbox_1{
+    color: #fff;
 }
 </style>
