bfd02162dfc097476d0ed5b3986dcf923ebc3465
seungh <seungh1024@naver.com>
2022-08-13 00:18:50 +0900

    Refactor-front:#/ update HOST with domain

--- backend/src/main/java/com/ssafy/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/backend/config/SecurityConfig.java
@@ -117,7 +117,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/api/v1/re-issue",
                         "/api/v1/email/**",
                         "/ws/**",
-//                        "/api/v1/user-info/**",
+                        "/api/v1/user-info/**",
                         "/api/v1/social/googleSignup",
                         "/api/v1/social/googleLogin"
 //                        "/api/v1/mbti/**",
