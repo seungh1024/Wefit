4331bd71f0c824e97b75a836876cff99dc1867cf
조성환 <chosw0812@naver.com>
2022-08-14 16:53:42 +0900

    nonloginview 수정

--- frontend/src/views/MainPage/NonLoginHome.vue
+++ frontend/src/views/MainPage/NonLoginHome.vue
@@ -1,6 +1,4 @@
 <template>
-<div class="Back">
-  <div class="main">
     <div class="background">
       <v-row>
           <div class="homebox">
@@ -16,8 +14,6 @@
         </button>
       </div>
       <div class="spacebox"></div>
-    </div>
-  </div>
 </div>
 </template>
 
@@ -28,19 +24,10 @@ export default {
 </script>
 
 <style scoped>
-/* 배경색 */
-.Back{
-  background-color: rgba(6, 6, 6, 6);
-}
-/* 메인 크기(1200기준으로 남는 부분 검정 채우기) */
-/* .main{
-  width:1200px; 
-  margin:0 auto; 
-} */
 
 /* 배경 사진 */
 .background{
-  height: 100%;
+  height: 100vh;
   width:100%;
   overflow: hidden;
   margin:0;
@@ -51,6 +38,7 @@ export default {
     rgba(6, 6, 6, 0),
         rgba(255, 255, 255, 0)
       ), url("@/assets/images/Home.png");
+      min-width: 1400px;
 }
 
 /* 상단 margin 처리 */
@@ -71,33 +59,12 @@ h1{
 }
 
 /* 반응형 */
-@media (min-width: 768px) {
-.homebox{
-  height: 10rem;
-}
-h1{
-  color:white;
-  font-size: 150px;
-  margin-left: 7rem;
-}
-}
-@media (min-width: 992px) {
-.homebox{
-  height: 15rem;
-}
-h1{
-  color:white;
-  font-size: 150px;
-}
-}
-@media (min-width: 1200px) {
 .homebox{
   height: 25rem;
 }
 h1{
   color:white;
   font-size: 150px;
-}
 }
 
 /* 아랫배경(색동일) */
@@ -136,34 +103,5 @@ button {
 
 .signupstyle:hover{
   color : black;
-}
-
-/* 설명상자(사진포함) */
-.whitebox{
-  display: flex;
-  background-color: aliceblue;
-}
-
-/* 설명상자(좌우대칭) */
-.whitebox-right{
-  display: flex;
-  background-color: aliceblue;
-  justify-content: space-between;
-}
-
-/* 부주제? */
-.sub-mention{
-  font-size: 50px;
-  color: aliceblue;
-  display: flex;
-  justify-content: center;
-}
-
-/* 글자 가운데 정렬(margin이용) */
-.div-center{
-  position: relative ;
-  margin-block: auto;
-  margin-inline: auto;
-  font-size : 50px;
 }
 </style>
